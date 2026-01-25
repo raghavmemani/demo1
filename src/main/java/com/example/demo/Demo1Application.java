de05ee7e045b2f59392afc8e78a4ededd60d8614
raghavmemani <memaniraghavdecember12@gmail.com>
2026-01-25 12:48:28 +0530

    REST API ADDED

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to javatechie !";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
